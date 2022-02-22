7d83766dd200b051fc8fdce5c38b111619bf9619
meher12 <meher.khemiri@gmail.com>
2022-02-22 14:03:50 +0100

    update bug

--- src/test/java/com/mdev/springboot/controller/ProductControllerTest.java
+++ src/test/java/com/mdev/springboot/controller/ProductControllerTest.java
@@ -33,59 +33,58 @@ import com.mdev.springboot.service.ProductService;
 @ExtendWith(MockitoExtension.class)
 public class ProductControllerTest {
 
-	// unit test
-	@Autowired
-	private MockMvc mockMvc;
+    // unit test
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Mock
-	private ProductService productService;
-	private Product product;
-	private List<Product> productList;
+    @Mock
+    private ProductService productService;
+    private Product product;
+    private List<Product> productList;
 
-	@InjectMocks
-	private ProductController productController;
+    @InjectMocks
+    private ProductController productController;
 
-	@BeforeEach
-	public void setup() {
-		product = new Product(1, "ball", 670);
-		mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
-	}
+    @BeforeEach
+    public void setup() {
+        product = new Product(1, "ball", 670);
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
 
-	@AfterEach
-	void tearDown() {
-		product = null;
-	}
+    @AfterEach
+    void tearDown() {
+        product = null;
+    }
 
-	// Test Case to Post a Product
-	@Test
-	public void PostMappingOfProduct() throws Exception {
-		when(productService.addProduct(any())).thenReturn(product);
-		mockMvc.perform(post("/api/v1/product").contentType(MediaType.APPLICATION_JSON).content(asJsonString(product)))
-				.andExpect(status().isCreated());
-		verify(productService, times(1)).addProduct(any());
-	}
+    // Test Case to Post a Product
+    @Test
+    public void PostMappingOfProduct() throws Exception {
+        when(productService.addProduct(any())).thenReturn(product);
+        mockMvc.perform(post("/api/v1/product").contentType(MediaType.APPLICATION_JSON).content(asJsonString(product)))
+                .andExpect(status().isCreated());
+        verify(productService, times(1)).addProduct(any());
+    }
 
-	// Test Case to Retrieve all Products
-	@Test
-	public void GetMappingOfAllProduct() throws Exception {
-		when(productService.getAllProducts()).thenReturn(productList);
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/products").contentType(MediaType.APPLICATION_JSON)
-				.content(asJsonString(product))).andDo(MockMvcResultHandlers.print());
-		verify(productService).getAllProducts();
-		verify(productService, times(1)).getAllProducts();
-	}
+    // Test Case to Retrieve all Products
+    @Test
+    public void GetMappingOfAllProduct() throws Exception {
+        when(productService.getAllProducts()).thenReturn(productList);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/products").contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(product))).andDo(MockMvcResultHandlers.print());
+        verify(productService).getAllProducts();
+        verify(productService, times(1)).getAllProducts();
+    }
 
     // Test Case to Retrieve Product by Id
-	@Test
-	public void GetMappingOfProductShouldReturnRespectiveProducct() throws Exception {
-		when(productService.getProductByid(product.getId())).thenReturn(product);
-		mockMvc.perform(get("/api/v1/product/1").contentType(MediaType.APPLICATION_JSON).content(asJsonString(product)))
-				.andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print());
-	}
-	
-	
-	// Test Case to Delete a Product
-	@Test
+    @Test
+    public void GetMappingOfProductShouldReturnRespectiveProducct() throws Exception {
+        when(productService.getProductByid(product.getId())).thenReturn(product);
+        mockMvc.perform(get("/api/v1/product/1").contentType(MediaType.APPLICATION_JSON).content(asJsonString(product)))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print());
+    }
+
+    // Test Case to Delete a Product
+    @Test
 //	public void DeleteMappingUrlAndIdThenShouldReturnDeletedProduct() throws Exception {
 //		when(productService.deleteProductById(product.getId())).thenReturn(product);
 //		mockMvc.perform(
@@ -93,11 +92,11 @@ public class ProductControllerTest {
 //				.andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print());
 //	}
 
-	public static String asJsonString(final Object obj) {
-		try {
-			return new ObjectMapper().writeValueAsString(obj);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
