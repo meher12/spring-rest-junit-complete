41337833dc4bdb69d8f8516bc451e65de5db52c9
meher12 <meher.khemiri@gmail.com>
2022-02-22 11:17:33 +0100

    add triggers pollSCM

--- src/main/java/com/mdev/springboot/exception/ProductAlreadyExistsException.java
+++ src/main/java/com/mdev/springboot/exception/ProductAlreadyExistsException.java
@@ -4,7 +4,7 @@ public class ProductAlreadyExistsException extends Exception{
     private String message;
 
     public ProductAlreadyExistsException(){
-        super();
+
     }
 
     public ProductAlreadyExistsException(String message){
